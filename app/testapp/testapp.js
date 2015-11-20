8d681318fcbb1287bdd8604b0b98a84fefcd6b31
Martijn Hermans <martijn@touchtribe.nl>
2015-11-20 16:23:20 +0100

    - added a simple 'lerp' to smooth out the pointers - added events being dispatched (handmove, handopen, handclose) - updated the testapp with a simple example

--- app/testapp/testapp.js
+++ app/testapp/testapp.js
@@ -17,33 +17,42 @@ class TestApp extends BaseApp
         this.$handRight = $('<div class="hand hand-right" />');
         this.$container.append(this.$handLeft).append(this.$handRight);
         this.trackingId = false;
-        this.touchSimulator;
-        this.bodyState = {
-            current: false,
-            previous: false
-        };
+	    this.trackingTimer = false;
+	    var _this = this;
         $(function(){
+	        ['handmove','handopen','handclose'].forEach(eventName => {
+		        document.addEventListener(eventName, function(event){
+			        clearTimeout(_this.trackingTimer);
+			        _this.trackingTimer = setTimeout(function(){
+				        _this.trackingId = false;
+			        },2500);
+			        _this.trackingId = _this.trackingId || event.trackingId;
+			        console.log(_this.trackingId);
+		        });
+	        });
             document.body.addEventListener('handmove',function(event){
-
-                var elem = document.querySelector('.hand.hand-' + event.name);
+				if (event.trackingId != _this.trackingId) return;
+                var elem = document.querySelector('.hand.hand-' + event.which);
                 elem.style.left = event.screenX + 'px';
                 elem.style.top = event.screenY + 'px';
             });
             document.body.addEventListener('handopen',function(event){
-                console.log('opened');
-                var elem = document.querySelector('.hand.hand-' + event.name);
+	            console.log('handopen');
+	            if (event.trackingId != _this.trackingId) return;
+	            var elem = document.querySelector('.hand.hand-' + event.which);
                 elem.style.backgroundColor = 'red';
             });
-            document.body.addEventListener('handclosed',function(event){
-                console.log('closed', Date.now());
-                var elem = document.querySelector('.hand.hand-' + event.name);
+            document.body.addEventListener('handclose',function(event){
+	            console.log('handclose');
+	            if (event.trackingId != _this.trackingId) return;
+	            var elem = document.querySelector('.hand.hand-' + event.which);
                 elem.style.backgroundColor = 'black';
             });
-        })
+        });
     }
     _bindSocket()
     {
-        this.websocket = new WebSocket('ws://10.111.100.101:1234/');
+        this.websocket = new WebSocket('ws://10.111.100.96:1234/');
         this.websocket.onmessage = this._handleMessage.bind(this);
     }
     onBodyData(bodyData)
--- includes/kinect-touch-pointer/kinect-touch-pointer.js
+++ includes/kinect-touch-pointer/kinect-touch-pointer.js
@@ -1,135 +1,166 @@
-var BodyData = require('../bodydata/bodydata'),
-    Three = require('three'),
+var Vector2 = require('three').Vector2,
+    Vector3 = require('three').Vector3,
     HandState = require('../bodydata/handstate'),
     JointType = require('../bodydata/jointtype');
 
 
-var temp2 = new Three.Vector2(),
-    temp3 = new Three.Vector3();
+var temp3 = new Vector3(),
+    lerpCount = 3;
+
+class KinectTouchPointerState
+{
+	constructor()
+	{
+		this.left = {
+			screenX: 0,
+			screenXHist: [],
+			screenY: 0,
+			screenYHist: [],
+			state: HandState.Unknown,
+			hasOpened: false,
+			hasMoved: false,
+			hasClosed: false
+		}
+		this.right = {
+			screenXHist: [],
+			screenY: 0,
+			screenYHist: [],
+			state: HandState.Unknown,
+			hasOpened: false,
+			hasMoved: false,
+			hasClosed: false
+		}
+	}
+	setState(which, state)
+	{
+		var cur = this[which];
+		cur.screenXHist.push(state.screenX);
+		cur.screenXHist = cur.screenXHist.slice(-lerpCount);
+		cur.screenX = cur.screenXHist.reduce((a,b)=>a+b,0) / cur.screenXHist.length;
+		cur.screenYHist.push(state.screenY);
+		cur.screenYHist = cur.screenYHist.slice(-lerpCount);
+		cur.screenY = cur.screenYHist.reduce((a,b)=>a+b,0) / cur.screenYHist.length;
+
+		cur.hasMoved = cur.screenX != state.screenX || cur.screenY != state.screenY;
+		cur.hasOpened = state.state == HandState.Open && cur.state != HandState.Open;
+		cur.hasClosed = state.state == HandState.Closed && cur.state != HandState.Closed;
+		cur.state = state.state;
+	}
+}
 
 class KinectTouchPointer
 {
-    constructor(id)
-    {
-        this.id = id;
+	constructor(id)
+	{
+		this.id = id;
+		this.states = {};
+	}
+	update(bodyData)
+	{
+		var screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+			screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		bodyData.bodies.forEach((body) => {
+			var armLength = this.getArmLength(body),
+			    boundingBox = [new Vector2(), new Vector2()],
+			    currentState = this._getState(body.trackingId),
+			    hand = new Vector2(),
+			    pos2 = new Vector2();
 
-        this.state = {
-            left:
-            {
-                screenX: 0,
-                screenY: 0,
-                state: HandState.Unknown
-            },
-            right:
-            {
-                screenX: 0,
-                screenY: 0,
-                state: HandState.Unknown
-            }
-        }
-    }
-    update(bodyData)
-    {
-        var body = bodyData.bodies.filter(body =>  body.trackingId = this.id);
-        if (body.length == 0)
-            return;
-        body = body[0];
-        var shoulder3d = this.get3dShoulderLength(body),
-            shoulder2d = this.get2dShoulderLength(body),
-            ratio = shoulder2d / shoulder3d,
-            armLength3 = this.get3dArmLength(body),
-            armLength2 = armLength3 * ratio,
-            screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-            screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-            leftHandZ = body.joints[JointType.HandLeft].position.z,
-            rightHandZ = body.joints[JointType.HandRight].position.z;
-
-        var midPoint2 = body.joints[JointType.SpineShoulder].color;
-        [['left', JointType.HandLeft], ['right',JointType.HandRight]].forEach(([name, jointType]) => {
-            var joint = body.joints[jointType],
-                pos2 = joint.color.clone();
-            pos2.sub(midPoint2).multiplyScalar(joint.position.z * 0.8);
-            pos2.x = (0.5 + (pos2.x * (0.8 * screenWidth / screenHeight))) * screenWidth;
-            pos2.y = (0.5 + (pos2.y)) * screenHeight;
-            var newState = body[name+'HandState'],
-                oldState = this.state[name];
+			// we are going to project the coordinates on a plane at Z = 1, which simplifies our * (planeZ / DepthZ) calculation to * (1 /  depthZ) = / depthZ
 
 
-            pos2.x = Math.max(0,Math.min(pos2.x,screenWidth));
-            pos2.y = Math.max(0,Math.min(pos2.y,screenHeight));
-            var evt = {
-                screenX: pos2.x,
-                screenY: pos2.y,
-                name: name,
-                target: document.elementFromPoint(pos2.x, pos2.y) || window,
-                isMoved: pos2.x != oldState.screenX || pos2.y != oldState.screenY,
-                isHandOpen: [HandState.Open, HandState.Unknown].indexOf(newState) >=0 && [HandState.Open, HandState.Unknown].indexOf(oldState.state) == -1,
-                isHandClosed: newState == HandState.Closed && oldState.state != HandState.Closed
-            };
-            oldState.screenX = evt.screenX;
-            oldState.screenY = evt.screenY;
-            oldState.state = newState;
-            this._dispatchEvent(evt);
-        });
-    }
-    _dispatchEvent(event){
-        var evt,
-            data = {
-                bubbles: true,
-                cancelable: true
-            };
-        if (event.isMoved)
-        {
-            evt = new CustomEvent('handmove', data);
-            evt.screenX = event.screenX;
-            evt.screenY = event.screenY;
-            evt.name = event.name;
-            evt.trackingId = this.id;
-            event.target.dispatchEvent(evt);
-        }
-        if (event.isHandOpen){
-            evt = new CustomEvent('handopen', data);
-            evt.screenX = event.screenX;
-            evt.screenY = event.screenY;
-            evt.name = event.name;
-            evt.trackingId = this.id;
-            event.target.dispatchEvent(evt);
-        }
-        if (event.isHandClosed)
-        {
-            evt = new CustomEvent('handclosed', data);
-            evt.screenX = event.screenX;
-            evt.screenY = event.screenY;
-            evt.name = event.name;
-            evt.trackingId = this.id;
-            event.target.dispatchEvent(evt);
-        }
-    }
-    get3dArmLength(body)
-    {
-        // first we calculate the length of the arms
-        var spine = body.joints[JointType.SpineShoulder],
-            shoulder = body.joints[JointType.ShoulderRight],
-            elbow = body.joints[JointType.ElbowRight],
-            hand = body.joints[JointType.HandRight],
-            length3 = 0;
-        length3 += temp3.subVectors(spine.position, shoulder.position).length();
-        length3 += temp3.subVectors(elbow.position, shoulder.position).length();
-        length3 += temp3.subVectors(elbow.position, hand.position).length();
-        return length3;
-    }
-    get3dShoulderLength(body)
-    {
-        var shoulderLeft = body.joints[JointType.ShoulderLeft],
-            shoulderRight = body.joints[JointType.ShoulderRight];
-        return temp3.subVectors(shoulderLeft.position, shoulderRight.position).length();
-    }
-    get2dShoulderLength(body)
-    {
-        var shoulderLeft = body.joints[JointType.ShoulderLeft],
-            shoulderRight = body.joints[JointType.ShoulderRight];
-        return temp2.subVectors(shoulderLeft.color, shoulderRight.color).length();
-    }
+
+			[['left', JointType.HandLeft, JointType.ShoulderLeft], ['right',JointType.HandRight, JointType.ShoulderRight]].forEach(([name, jointTypeHand, jointTypeShoulder]) => {
+				var jntHand = body.joints[jointTypeHand],
+				    jntShoulder = body.joints[jointTypeShoulder],
+				    armLength = this.getArmLength(body,name) / 1.4;
+				boundingBox[0].x = (jntShoulder.position.x - armLength) /  jntShoulder.position.z;
+				boundingBox[0].y = (jntShoulder.position.y - armLength) /  jntShoulder.position.z;
+				boundingBox[1].x = (jntShoulder.position.x + armLength) /  jntShoulder.position.z;
+				boundingBox[1].y = (jntShoulder.position.y + armLength) /  jntShoulder.position.z;
+				hand.x = jntHand.position.x / jntHand.position.z;
+				hand.y = jntHand.position.y / jntHand.position.z;
+
+				pos2.x = ((hand.x - boundingBox[0].x) / (boundingBox[1].x - boundingBox[0].x)) * screenWidth;// / (screenWidth / screenHeight);
+				pos2.y = screenHeight - (((hand.y - boundingBox[0].y) / (boundingBox[1].y - boundingBox[0].y)) * screenHeight);
+
+				currentState.setState(name, {
+					screenX: Math.min(screenWidth - 1,Math.max(1,pos2.x)),
+					screenY: Math.min(screenHeight - 1,Math.max(1,pos2.y)),
+					state: body[name+'HandState']
+				});
+
+				var currentHandState = currentState[name];
+
+				this._dispatchEvent({
+					screenX: currentHandState.screenX,
+					screenY: currentHandState.screenY,
+					name: name,
+					target: document.elementFromPoint(currentHandState.screenX, currentHandState.screenY) || window,
+					tackingId: body.trackingId,
+					hasMoved: currentHandState.hasMoved,
+					hasHandOpened: currentHandState.hasOpened,
+					hasHandClosed: currentHandState.hasClosed
+				});
+			});
+		});
+
+	}
+	_cleanUp(bodyData)
+	{
+		var ids = bodyData.bodies.map(body => body.trackingId);
+		this.states.keys().forEach(id => {
+			if (! ids.indexOf(id)) return;
+			this.states[ids] = null;
+			delete this.states[ids];
+		})
+	}
+	_getState(id)
+	{
+		if (! this.states.hasOwnProperty(id)) {
+			this._setState(id,new KinectTouchPointerState());
+		}
+		return this.states[id];
+	}
+	_setState(id, state)
+	{
+		this.states[id] = state;
+	}
+	_dispatchEvent(event){
+		var evt,
+		    data = {
+			    bubbles: true,
+			    cancelable: true
+		    };
+		[   [event.hasMoved, 'handmove'],
+			[event.hasHandOpened,'handopen'],
+			[event.hasHandClosed,'handclose']
+		].forEach(([hasEvent,eventType])=>{
+			if (! hasEvent) return;
+
+			evt = new CustomEvent(eventType, data);
+			evt.screenX = event.screenX;
+			evt.screenY = event.screenY;
+			evt.which = event.name;
+			evt.trackingId = this.id;
+			event.target.dispatchEvent(evt);
+		});
+
+	}
+	getArmLength(body,which)
+	{
+		which = which == 'left'?'Left':'Right';
+		// first we calculate the length of the arms
+		var spine = body.joints[JointType.SpineShoulder],
+		    shoulder = body.joints[JointType['Shoulder'+which]],
+		    elbow = body.joints[JointType['Elbow'+which]],
+		    hand = body.joints[JointType['Hand'+which]],
+		    length3 = 0;
+		length3 += temp3.subVectors(spine.position, shoulder.position).length();
+		length3 += temp3.subVectors(elbow.position, shoulder.position).length();
+		length3 += temp3.subVectors(elbow.position, hand.position).length();
+		return length3;
+	}
 }
 
 module.exports = KinectTouchPointer;
